80a08c6bd354a326ad723bdd0843866176561b49
itsjustyagmur <44810927+itsjustyagmur@users.noreply.github.com>
2022-08-09 16:50:52 -0500

    your commit message

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import BrowserRouter from 'react-router-dom/BrowserRouter'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -9,3 +10,8 @@ root.render(
     <App />
   </React.StrictMode>
 );
+ReactDOM.render((
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <App />
+  </BrowserRouter>
+),)  
