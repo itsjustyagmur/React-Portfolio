bbaa62b5857a4eb3d0ca758c2ab2d4d2c464dffa
itsjustyagmur <44810927+itsjustyagmur@users.noreply.github.com>
2022-08-15 22:20:13 -0500

    new

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -12,31 +12,31 @@ export default function Portfolio() {
         
       </div>
        <div className="item">
-          <img src="./assets/clock.png" alt=""/>
+          <img src="assets/clock.png" alt=""/>
           <a href="https://itsjustyagmur.github.io/javaclock/">Clock</a>
         </div>
         <div className="item">
-          <img src="./assets/quote.png" alt=""/>
+          <img src="assets/quote.png" alt=""/>
           <a href="https://itsjustyagmur.github.io/quote/">Zen</a>
         </div>
         <div className="item">
-          <img src="./assets/broadway.png" alt=""/>
+          <img src="assets/broadway.png" alt=""/>
           <a href="https://itsjustyagmur.github.io/broadwaywebsite/">Broadway</a>
         </div>
         <div className="item">
-          <img src="./assets/morroco.png" alt=""/>
+          <img src="assets/morroco.png" alt=""/>
           <a href="https://itsjustyagmur.github.io/Morocco/">Morocco</a>
         </div>
         <div className="item">
-          <img src="./assets/trivia.jpg" alt=""/>
+          <img src="assets/trivia.jpg" alt=""/>
           <a href="https://itsjustyagmur.github.io/trivia-game/">Trivia</a>
         </div>
         <div className="item">
-          <img src="./assets/classvue.png" alt=""/>
+          <img src="assets/classvue.png" alt=""/>
           <a href="https://itsjustyagmur.github.io/evileye/">ClassVue</a>
         </div>
         <div className="item">
-          <img src="./assets/wordguess.jpg" alt=""/>
+          <img src="assets/wordguess.jpg" alt=""/>
           <a href="https://itsjustyagmur.github.io/Word-Guess-Game/">Guess</a>
         </div>
        
